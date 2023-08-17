d895febdbaf912bfff27a5bc86b98c10e74ed5a3
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-08-17 15:49:32 -0500

    subida de archivos con inputs de tareas y configuración de firebase

---
+++ tareas/src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKZ2Ka2yjMauGXy-V3vbhPXAETUtxSgtQ",
+  authDomain: "todo-48ccb.firebaseapp.com",
+  projectId: "todo-48ccb",
+  storageBucket: "todo-48ccb.appspot.com",
+  messagingSenderId: "762687333441",
+  appId: "1:762687333441:web:66d8bd931ed9609d4092c0",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+console.log("Firebase");
+export default db;
