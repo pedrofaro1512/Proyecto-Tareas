1fd832760c85a091c812099edb8c065b6a3eaed0
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-08-18 20:03:19 -0500

    Adición de check en tareas

--- tareas/src/App.js
+++ tareas/src/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
 import db from "./utils/firebase";
 import { collection, getDocs, orderBy } from "firebase/firestore";
 import "firebase/firestore";
 import AddTask from "./components/AddTask/AddTask";
+import Task from "./components/Task/Task";
+import { map, size } from "lodash";
 
 import "./App.scss";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  console.log(tasks);
+  const [tasks, setTasks] = useState(null);
+  const [reloadTask, setReloadTask] = useState(false);
 
   useEffect(() => {
     const obtenerDatos = async () => {
       const datos = await getDocs(collection(db, "task"), orderBy("completed"));
-      // console.log(datos.docs[0].data());
+
       const arrayTasks = [];
       datos.forEach((doc) => {
         const data = doc.data();
@@ -24,7 +26,8 @@ function App() {
       setTasks(arrayTasks);
     };
     obtenerDatos();
-  }, []);
+    setReloadTask(false);
+  }, [reloadTask]);
 
   return (
     <Container fluid className="app">
@@ -45,14 +48,25 @@ function App() {
           xs={{ span: 10, offset: 1 }}
           md={{ span: 6, offset: 3 }}
         >
-          <p>Lista de tareas</p>
+          {!tasks ? (
+            <div className="loading">
+              <Spinner animation="border" />
+              <span>Cargando...</span>
+            </div>
+          ) : size(tasks) === 0 ? (
+            <h3>No hay tareas</h3>
+          ) : (
+            map(tasks, (task, index) => (
+              <Task key={index} task={task} setReloadTask={setReloadTask} />
+            ))
+          )}
         </Col>
         <Col
           className="todo__input"
           xs={{ span: 10, offset: 1 }}
           md={{ span: 6, offset: 3 }}
         >
-          <AddTask />
+          <AddTask setReloadTask={setReloadTask} reloadTask={reloadTask} />
         </Col>
       </Row>
     </Container>
---
+++ tareas/src/components/Task/Task.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ReactComponent as Check } from "../../assets/check.svg";
+import { ReactComponent as Delete } from "../../assets/delete.svg";
+import db from "../../utils/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import "firebase/firestore";
+
+import "./Task.scss";
+
+const Task = (props) => {
+  const { task, setReloadTask } = props;
+
+  const completeTask = async () => {
+    const taskRef = doc(db, "task", task.id);
+
+    await updateDoc(taskRef, {
+      completed: !task.completed,
+    });
+    setReloadTask(true);
+  };
+
+  return (
+    <div className="task">
+      <div>
+        <Check
+          className={task.completed ? "completed" : ""}
+          onClick={completeTask}
+        />
+      </div>
+      <div>{task.name}</div>
+      <div>
+        <Delete />
+      </div>
+    </div>
+  );
+};
+
+export default Task;
